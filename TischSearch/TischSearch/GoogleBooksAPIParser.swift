f146e07e0efbb046526fdcc99aec93c3a26e4093
Mike Yeung <mikeyeung123@gmail.com>
2015-04-08 21:19:53 -0400

    Created Google Books API Parser class

---
+++ TischSearch/TischSearch/GoogleBooksAPIParser.swift
@@ -0,0 +1,13 @@
+//
+//  GoogleBooksAPIParser.swift
+//  TischSearch
+//
+//  Created by Mike Yeung on 4/8/15.
+//  Copyright (c) 2015 Mike Yeung. All rights reserved.
+//
+
+import Foundation
+
+class GoogleBooksAPIParser {
+    
+}
